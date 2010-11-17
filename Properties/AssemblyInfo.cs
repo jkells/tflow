05a4939c3473fe8cbb9a4c17699522a3dcc6ed2a
Jared Kells <jkells@gmail.com>
2010-11-17 23:19:41 +1000

    Set version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Download speed analysis tool")]
 [assembly: AssemblyCompany("Jared Kells")]
 [assembly: AssemblyProduct("TFlow")]
-[assembly: AssemblyCopyright("Copyright © Jared Kells 2010")]
+[assembly: AssemblyCopyright("Copyright � Jared Kells 2010")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: ComVisible(false)]
